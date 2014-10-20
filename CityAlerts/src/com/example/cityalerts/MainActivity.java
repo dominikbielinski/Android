95fa1b3b672d5ea5fc2a654f1b7eb50dda00f03d
Dominik <Dominik@Dominik_01.pl>
2014-10-20 19:23:27 +0200

    FullscreenKorekta

--- CityAlerts/src/com/example/cityalerts/MainActivity.java
+++ CityAlerts/src/com/example/cityalerts/MainActivity.java
@@ -13,12 +13,12 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
+		setContentView(R.layout.activity_main);
 	}
 
 	@Override
